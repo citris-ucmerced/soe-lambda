b54a8dc42dc8f0e17eb811526b61d9782c2fdaca
Denis <denisvashchenko@gmail.com>
2019-11-10 16:54:53 -0800

    adding serverless asp.net

---
+++ soe_lambda/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace soe_lambda.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpPost]
+        public JObject Get([FromBody] JObject input)
+        {
+            return input;
+        }
+    }
+}
